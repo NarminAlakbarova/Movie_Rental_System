680326c07a7de466ffd76561828b0e054e49216b
NarminAlakbarova <735e5lb@code.edu.az>
2023-06-13 21:01:53 +0400

    A sort and search function was added to the films page

--- admin/js/films.js
+++ admin/js/films.js
@@ -1,3 +1,7 @@
+
+
+
+// COUNTER
 let a = 0;
 $(document).ready(function () {
   $(".count").each(function () {
@@ -15,7 +19,11 @@ $(document).ready(function () {
   });
   a = 1;
 });
-let filteredData = [];
+
+
+let dataArr = [];
+let copyArr = [];
+let sortedArr = [];
 let tBody = document.querySelector("tbody");
 let count = 0;
 const BASE_URL = "http://localhost:8080";
@@ -51,22 +59,40 @@ function drawTabele(arr) {
   });
 }
 // GETDATA
+let searchInp = document.querySelector(".search");
 async function getAllData() {
   let resp = await axios(`${BASE_URL}/allMovies`);
-  let data = resp.data;
-  filteredData = data;
-  drawTabele(data);
+  dataArr = resp.data;
+  copyArr = searchInp.value ? copyArr : resp.data;
+  drawTabele(copyArr);
 }
 getAllData();
+
+
+// SEARCH
+searchInp.addEventListener("input", function (e) {
+  copyArr = dataArr;
+  copyArr = copyArr.filter((item) =>
+    item.movieName
+      .toLocaleLowerCase()
+      .includes(e.target.value.toLocaleLowerCase())
+  );
+  drawTabele(copyArr);
+});
+
+
 // DELETE
 async function deleteMovies(moviesId) {
   await axios.delete(`${BASE_URL}/allMovies/${moviesId}`);
 
   document.querySelector(`#${moviesId}`).closest("tr").remove();
 }
+
+
 // MORE DETAILS
 function showMoreDetails(userId) {
-  let findFilm = filteredData.find((item) => item.id == userId);
+    console.log(userId);
+  let findFilm = copyArr.find((item) => item.id == userId);
   let modalBody = document.getElementById("userModalBody");
   modalBody.innerHTML = `
           <div class="img"><img src="${findFilm.img}"></div>
@@ -89,3 +115,35 @@ function showMoreDetails(userId) {
     userModal.hide();
   });
 }
+
+// SORT
+
+let dropdown = document.querySelector(".sortDrop");
+dropdown.addEventListener("click", function (event) {
+  console.log("hello");
+  let target = event.target;
+  if (target.classList.contains("dropdown-item")) {
+    sortMovies(target.innerText);
+  }
+});
+
+function sortMovies(sortType) {
+  if (sortType === "Default") {
+    copyArr = dataArr;
+  } else if (sortType === "imdb rated- highest") {
+    copyArr = copyArr.toSorted((a, b) => b.imbd - a.imbd);
+  } else if (sortType === "imdb rated - lowest") {
+    copyArr = copyArr.toSorted((a, b) => a.imbd - b.imbd);
+  } else if (sortType === "A-Z") {
+    copyArr = copyArr.toSorted((a, b) =>
+      a.movieName.localeCompare(b.movieName)
+    );
+  } else if (sortType === "Z-A") {
+    copyArr = copyArr.toSorted((a, b) =>
+      b.movieName.localeCompare(a.movieName)
+    );
+  }
+  sorted = copyArr;
+  drawTabele(copyArr);
+}
+sortMovies();
