b69ae1a15175cb3e9ff4420bb4db855a33c050fe
NarminAlakbarova <735e5lb@code.edu.az>
2023-06-17 20:01:29 +0400

    The back to top function was written to common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -50,3 +50,17 @@ function scrollFunction() {
   });
   
 
+// BACT TO TOP
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#scroll').fadeIn(); 
+      } else { 
+          $('#scroll').fadeOut(); 
+      } 
+  }); 
+  $('#scroll').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+      return false; 
+  }); 
+});
