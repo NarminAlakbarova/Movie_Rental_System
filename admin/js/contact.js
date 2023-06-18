8475bb6cc4dfbe8182f2b348cefb62f2545168c5
NarminAlakbarova <735e5lb@code.edu.az>
2023-06-19 01:06:53 +0400

    Delete function was added to the contact page in the admin

--- admin/js/contact.js
+++ admin/js/contact.js
@@ -1,16 +1,17 @@
-let contactRow=document.querySelector(".contact-row")
+let contactRow = document.querySelector(".contact-row");
 
 const BASE_URL = "http://localhost:8080";
 
-function drawRow(arr){
-    contactRow.innerHTML=""
-    arr.forEach((item)=>{
-        contactRow.innerHTML+=`
+function drawRow(arr) {
+  contactRow.innerHTML = "";
+  arr.forEach((item) => {
+    contactRow.innerHTML += `
         
-        <div class="col-lg-3 my-2">
+        <span class="col-lg-3 my-2">
         <div class="card">
           <div class="avatar">
             <img src="../assets/img/users/pic-4.png" alt="Avatar" />
+            <i class="fa-regular fa-trash-can" onclick=deleteContactCard(${item.id})></i>
           </div>
           <div class="message-content">
             <div class="sender">${item.username}</div>
@@ -22,16 +23,20 @@ function drawRow(arr){
           <div class="back-card"></div>
           <div class="thid-card"></div>
         </div>
-      </div>
+      </span>
         
-        `
-    })
-}async function getAllData(){
-    let resp=await axios(`${BASE_URL}/contact`)
-    let data=resp.data
-
-    drawRow(data)
+        `;
+  });
 }
-getAllData()
+async function getAllData() {
+  let resp = await axios(`${BASE_URL}/contact`);
+  let data = resp.data;
 
+  drawRow(data);
+}
+getAllData();
 
+async function deleteContactCard(id) {
+  await axios.delete(`${BASE_URL}/contact/${id}`);
+  document.querySelector(`#${id}`).closest("span").remove();
+}
