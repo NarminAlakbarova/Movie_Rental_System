7e75823190fdecb11ba3c5174f62525cacfff10c
NarminAlakbarova <735e5lb@code.edu.az>
2023-06-18 16:42:11 +0400

    dark mode has been added to the admin page

--- admin/js/common.js
+++ admin/js/common.js
@@ -1,10 +1,25 @@
-let countMessage=document.querySelector(".count")
+let countMessage = document.querySelector(".count");
 const CONTACT_URL = "http://localhost:8080";
 async function favCaunter() {
-    let resp = await axios(`${CONTACT_URL}/contact`);
-    let data = resp.data;
-    let newFavCounter = data.length;
-  
-    countMessage.innerHTML = newFavCounter.toString();
+  let resp = await axios(`${CONTACT_URL}/contact`);
+  let data = resp.data;
+  let newFavCounter = data.length;
+
+  countMessage.innerHTML = newFavCounter.toString();
+}
+favCaunter();
+
+// DARK MODE
+const sunIcon = document.querySelector(".fa-sun");
+const body = document.body;
+let isDarkMode = false;
+sunIcon.addEventListener("click", toggleDarkMode);
+function toggleDarkMode() {
+  if (isDarkMode) {
+    body.classList.add("dark-mode");
+    isDarkMode = false;
+  } else {
+    body.classList.remove("dark-mode");
+    isDarkMode = true;
   }
-  favCaunter();
+}
