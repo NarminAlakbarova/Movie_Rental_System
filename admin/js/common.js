aea6d4ceb304a01fcea2538ff82aedb1c27987e0
NarminAlakbarova <735e5lb@code.edu.az>
2023-06-19 21:20:46 +0400

    dark mode was written with localStorage

--- admin/js/common.js
+++ admin/js/common.js
@@ -14,7 +14,19 @@ const sunIcon = document.querySelector(".fa-sun");
 const body = document.body;
 sunIcon.addEventListener("click", function () {
   body.classList.toggle("dark-mode");
+  let mode = body.classList.contains("dark-mode") ? "dark" : "light";
   sunIcon.classList.contains("fa-sun")
     ? (this.classList = "fa-solid fa-moon")
     : (this.classList = "fa-solid fa-sun");
+  localStorage.setItem("pageMode", JSON.stringify(mode));
 });
+
+function getModeFunc() {
+  let getMode = JSON.parse(localStorage.getItem("pageMode"));
+  if (getMode === "dark") {
+    body.classList = "dark-mode";
+    sunIcon.classList = "fa-solid fa-moon";
+  }
+}
+getModeFunc();
+
