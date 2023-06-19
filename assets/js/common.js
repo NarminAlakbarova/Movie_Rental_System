2b515e6a98dc1518d38f091045eba1107f035009
NarminAlakbarova <735e5lb@code.edu.az>
2023-06-19 19:11:30 +0400

    updated common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,25 +1,28 @@
-// let burgerMenu = document.querySelector("#burger-menu");
+let burgerMenu = document.getElementById("burger-menu");
 let markIcon = document.querySelector(".fa-xmark");
 let menuIcon = document.querySelector(".fa-bars");
 let header = document.querySelector("header");
 let searchInp = document.querySelector(".search-input");
 let searchIcon = document.querySelector(".fa-search");
 let dropdown = document.querySelector(".dropdown");
-
+let basketCount = document.querySelector(".basket-count");
+let logOutIcon = document.querySelector(".log-out");
+let userNameSpan = document.querySelector(".user-name");
+const USER_URL = "http://localhost:8080";
+let allPremiumMovies2 = JSON.parse(localStorage.getItem("premiumMovies")) || [];
+let allDataCommon=[]
 // DROPDOWN
 dropdown.addEventListener("mouseenter", function () {
   dropdown.setAttribute("data-bs-toggle", "");
   dropdown.removeAttribute("aria-expanded");
 });
-
 dropdown.addEventListener("mouseleave", function () {
   dropdown.setAttribute("data-bs-toggle", "dropdown");
   dropdown.setAttribute("aria-expanded", "false");
 });
 
 // FIND USER
-let userNameSpan = document.querySelector(".user-name");
-const USER_URL = "http://localhost:8080";
+
 function drawUserName(obj) {
   userNameSpan.innerHTML = "";
   userNameSpan.innerHTML = `${obj ? obj.userName : ''}`;
@@ -28,24 +31,23 @@ function drawUserName(obj) {
 async function findUser() {
   let resp = await axios(`${USER_URL}/users`);
   let data = resp.data;
+  allDataCommon=data
   console.log(data);
-  let user = data.find((item) => item.check === true);
+  let user = allDataCommon.find((item) => item.check === true);
   console.log(user);
   drawUserName(user);
 }
 findUser();
-// BURGET MENU
-const burgerMenu = document.getElementById("burger-menu");
-jQuery(document).ready(function () {
-  var body = jQuery(document.body);
-  var button = jQuery("svg");
-  var line = jQuery("line");
 
+
+// BURGET MENU
+jQuery(document).ready(function () {
+  let body = jQuery(document.body);
+  let button = jQuery("svg");
   button.click(function () {
     if (jQuery(document.body).hasClass("menu-open")) {
       body.removeClass("menu-open");
       burgerMenu.classList.remove("show");
-
       return;
     }
     burgerMenu.classList.add("show");
@@ -56,8 +58,8 @@ jQuery(document).ready(function () {
 //   SCROLL
 function scrollFunction() {
   if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
+    document.body.scrollTop > 50 ||
+    document.documentElement.scrollTop > 50
   ) {
     header.style.background = "#141414";
   } else {
@@ -86,12 +88,9 @@ $(document).ready(function () {
 
 // LOG-OUT 
 
-let logOutIcon = document.querySelector(".log-out");
-logOutIcon.addEventListener("click", async function () {
-  let resp = await axios(`${USER_URL}/users`);
 
-  let data = resp.data;
-  let users = data.find((user) => user.check === true);
+logOutIcon.addEventListener("click", async function () {
+  let users = allDataCommon.find((user) => user.check === true);
   console.log(users);
   await axios.patch(`http://localhost:8080/users/${users.id}`, {
     check: false,
@@ -99,12 +98,8 @@ logOutIcon.addEventListener("click", async function () {
   localStorage.clear();
 });
 
-let allPremiumMovies2 = JSON.parse(localStorage.getItem("premiumMovies")) || [];
-let basketCount = document.querySelector(".basket-count");
-console.log(allPremiumMovies2);
-
+// BASKET COUNTER
 function basketCounter() {
-
   let counter = allPremiumMovies2.length;
   console.log(counter);
   if (basketCount) {
@@ -112,7 +107,6 @@ function basketCounter() {
     console.log(counter);
   }
 }
-
 basketCounter();
 
 // SPINNER
